6f7f2c962be5974c93db834bd14b59cb4a1c30ad
DxsGeorge <georgepetrg@gmail.com>
2017-01-30 19:11:29 +0200

    Hough & Canny trackbars
    Added trackbars to test hough and canny parameters

--- CV/main.cpp
+++ CV/main.cpp
@@ -15,22 +15,39 @@ using namespace std;
 
 int main()
 {
-	int p_offset = 5; //offset for checking for same points
-	float l_offset = 0.1; //offset for checking for vertical lines
-	int s_offset=5; //offset for checking for same lines
-	int canny_p[3]={11,33,3};
-	int hough_p[3]={100,100,10};
+	//Define Algorith Margins
+
+	int p_offset = 5; //limit of checking for same points
+	float l_offset = 0.1; //limit of checking for vertical lines
+	int s_offset=5; //limit of checking for same lines
+	int canny_p[3]={50,200,3}; //Canny parameters	
+	int hough_p[3]={50,100,50}; //HoughLinesP parameters
+	int C_lowThreshold=50;
+	int H_lowThreshold1=100;
+	int H_lowThreshold2=100;
+	int H_lowThreshold3=50;
+	int const max_lowThreshold = 100;
+	int const max_H_Threshold = 200;
+	namedWindow("video",1);
+	namedWindow("Canny",1);
+	createTrackbar("Canny:", "Canny", &C_lowThreshold, max_lowThreshold);
+	createTrackbar("Hough 1:", "Canny", &H_lowThreshold1, max_H_Threshold);
+	createTrackbar("Hough 2:", "Canny", &H_lowThreshold2, max_H_Threshold);
+	createTrackbar("Hough 3:", "Canny", &H_lowThreshold3, max_H_Threshold);
 	//Video capture
 
 	VideoCapture cap(1);
 	Mat src_copy;
 	while (true)
 	{
+		int canny_p[3]={C_lowThreshold,C_lowThreshold*3,3};
+		int hough_p[3]={H_lowThreshold1,H_lowThreshold2,H_lowThreshold3};
 		Mat src;
-		cap >> src; // get a new frame from camera
+		cap >> src; 
 		flip(src,src,1);
 
-		//Hough 
+		//HoughTransform on captured frame
+
 		if (!src.empty())
 		{
 			src.copyTo(src_copy);
@@ -51,7 +68,7 @@ int main()
 			SamePoints=FindIntersectionSpots(lines,p_offset,l_offset);
 
 			//Draw
-	
+			cvtColor(dst,dst,CV_GRAY2BGR);
 			for (size_t i=0;i<lines.size();i++)
 			{
 				line( dst, Point(lines[i].x1, lines[i].y1), Point(lines[i].x2, lines[i].y2), Scalar(0,0,255), 1, CV_AA);
@@ -64,22 +81,11 @@ int main()
 				circle (dst, SamePoints[i], 1, Scalar(0,255,0), 2);
 				putText(dst,text1, SamePoints[i],FONT_HERSHEY_SIMPLEX,0.33,Scalar(0,255,0));
 			}
-		
-			//imshow("video",src);
-			imshow("canny",dst);
+			
+			imshow("video",src);
+			imshow("Canny",dst);
 		}
 		if( waitKey(1) == 27 ) break;
-		Sleep(66);
+		Sleep(33);
 	}
-	
-
-	
-
-	
-	/*
-	imshow("source",src_copy);
-	imshow("Hough", src);
-	imshow("Canny", dst);
-	*/
-
 }
